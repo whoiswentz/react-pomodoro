39cf7560889a93387b8daae1c8fedf895309b7b8
Vinicios Wentz <vinicios@wentz.io>
2023-10-21 09:44:13 -0300

    feat(router): introduced react router lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
 import { GlobalStyle } from "./global";
+import { Router } from "./Router";
 import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <Button color='primary' />
-      <Button color='secondary' />
-      <Button color='success' />
-      <Button color='danger' />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
